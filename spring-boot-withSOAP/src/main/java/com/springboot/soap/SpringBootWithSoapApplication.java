f62d491313d39a6bfa21e0534ef99116882dabf4
nagarjuna <nagarjuna@nagarjunaa>
2020-02-07 13:46:02 +0530

    First Commit

---
+++ spring-boot-withSOAP/src/main/java/com/springboot/soap/SpringBootWithSoapApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSoapApplication.class, args);
+	}
+
+}
---
+++ spring-boot-withSOAP/src/test/java/com/springboot/soap/SpringBootWithSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
